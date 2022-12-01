3a7f3545025e9cc9b6d43b880a2b7f1abf7de4b5
jungwolf <75339254+jungwolf@users.noreply.github.com>
2022-12-01 11:26:35 -0600

    Update example_input_inserts.sql

--- Day01/Oracle/example_input_inserts.sql
+++ Day01/Oracle/example_input_inserts.sql
@@ -5,6 +5,21 @@ drop table day01_example;
 create table day01_example (lineno number, linevalue varchar2(4000));
 create synonym input_data for day01_example;
 
---insert into input_data (lineno, linevalue) values (line_number_sq.nextval,'');
+--insert into input_data (lineno, linevalue) values (line_number_sq.nextval,'
+--');
+insert into input_data (lineno, linevalue) values (line_number_sq.nextval,'1000');
+insert into input_data (lineno, linevalue) values (line_number_sq.nextval,'2000');
+insert into input_data (lineno, linevalue) values (line_number_sq.nextval,'3000');
+insert into input_data (lineno, linevalue) values (line_number_sq.nextval,'');
+insert into input_data (lineno, linevalue) values (line_number_sq.nextval,'4000');
+insert into input_data (lineno, linevalue) values (line_number_sq.nextval,'');
+insert into input_data (lineno, linevalue) values (line_number_sq.nextval,'5000');
+insert into input_data (lineno, linevalue) values (line_number_sq.nextval,'6000');
+insert into input_data (lineno, linevalue) values (line_number_sq.nextval,'');
+insert into input_data (lineno, linevalue) values (line_number_sq.nextval,'7000');
+insert into input_data (lineno, linevalue) values (line_number_sq.nextval,'8000');
+insert into input_data (lineno, linevalue) values (line_number_sq.nextval,'9000');
+insert into input_data (lineno, linevalue) values (line_number_sq.nextval,'');
+insert into input_data (lineno, linevalue) values (line_number_sq.nextval,'10000');
 
 commit;

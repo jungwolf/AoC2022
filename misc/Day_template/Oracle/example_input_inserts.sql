67e868b5894d823369755ebf9502007e8c98b363
Steven Patenaude <spatenaude@netspend.com>
2022-12-02 17:04:28 -0600

    standardizing create synonym for template files

--- misc/Day_template/Oracle/example_input_inserts.sql
+++ misc/Day_template/Oracle/example_input_inserts.sql
@@ -1,11 +1,10 @@
 drop sequence line_number_sq;
 create sequence line_number_sq;
 
-drop table   dayXX_example;
-create table dayXX_example (lineno number, linevalue varchar2(4000));
+drop table   day??_example;
+create table day??_example (lineno number, linevalue varchar2(4000));
 
-drop synonym input_data; 
-create synonym input_data for dayXX_example;
+create or replace synonym input_data for day??_example;
 
 --insert into input_data (lineno, linevalue) values (line_number_sq.nextval,'
 --');
--- misc/Day_template/Oracle/part1_input_inserts.sql
+++ misc/Day_template/Oracle/part1_input_inserts.sql
@@ -1,11 +1,10 @@
 drop sequence line_number_sq;
 create sequence line_number_sq;
 
-drop table   dayXX_part1;
-create table dayXX_part1 (lineno number, linevalue varchar2(4000));
+drop table   day??_part1;
+create table day??_part1 (lineno number, linevalue varchar2(4000));
 
-drop synonym input_data; 
-create synonym input_data for dayXX_part1;
+create or replace synonym input_data for day??_part1;
 
 --insert into input_data (lineno, linevalue) values (line_number_sq.nextval,'
 --');

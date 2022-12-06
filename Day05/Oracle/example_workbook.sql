a22071922939591a118600c771c59fe11a7558a7
Steven Patenaude <spatenaude@netspend.com>
2022-12-06 17:30:52 -0600

    playing around for example solution

--- Day05/Oracle/example_workbook.sql
+++ Day05/Oracle/example_workbook.sql
@@ -3,4 +3,40 @@ create or replace synonym input_data for day05_example;
 
 select * from input_data;
 
--- Sample
+-- quick group to separate data from instructions
+with g as (
+  select lineno,linevalue
+    , sum(nvl2(linevalue,0,1)) over (order by lineno) grouping
+  from input_data
+)
+select *
+from g;
+
+/*
+    [D]
+[N] [C]
+[Z] [M] [P]
+ 1   2   3
+
+what if we flip it? something like:
+[...
+1ZN
+[...
+[...
+2MCD
+[...
+[...
+3P
+[...
+
+how to do that?
+*/
+-- create a view so I can focus on the idea
+create or replace view group0 as
+select lineno, linevalue from (
+  select lineno,linevalue
+    , sum(nvl2(linevalue,0,1)) over (order by lineno) grouping
+  from input_data
+) where grouping = 0
+/
+select * from group0 order by lineno desc;

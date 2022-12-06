bc14f67d2cad4498542f0bbdd3514538c83f8073
Steven Patenaude <spatenaude@netspend.com>
2022-12-06 17:06:38 -0600

    example setup

--- Day05/Oracle/example_input_inserts.sql
+++ Day05/Oracle/example_input_inserts.sql
@@ -1,12 +1,21 @@
 drop sequence line_number_sq;
 create sequence line_number_sq;
 
-drop table   day??_example;
-create table day??_example (lineno number, linevalue varchar2(4000));
+drop table   day05_example;
+create table day05_example (lineno number, linevalue varchar2(4000));
 
-create or replace synonym input_data for day??_example;
+create or replace synonym input_data for day05_example;
 
 --insert into input_data (lineno, linevalue) values (line_number_sq.nextval,'
 --');
+insert into input_data (lineno, linevalue) values (line_number_sq.nextval,'    [D]    ');
+insert into input_data (lineno, linevalue) values (line_number_sq.nextval,'[N] [C]    ');
+insert into input_data (lineno, linevalue) values (line_number_sq.nextval,'[Z] [M] [P]');
+insert into input_data (lineno, linevalue) values (line_number_sq.nextval,' 1   2   3 ');
+insert into input_data (lineno, linevalue) values (line_number_sq.nextval,'');
+insert into input_data (lineno, linevalue) values (line_number_sq.nextval,'move 1 from 2 to 1');
+insert into input_data (lineno, linevalue) values (line_number_sq.nextval,'move 3 from 1 to 3');
+insert into input_data (lineno, linevalue) values (line_number_sq.nextval,'move 2 from 2 to 1');
+insert into input_data (lineno, linevalue) values (line_number_sq.nextval,'move 1 from 1 to 2');
 
 commit;
--- Day05/Oracle/example_workbook.sql
+++ Day05/Oracle/example_workbook.sql
@@ -1,5 +1,5 @@
 -- scratchpad for creating the solution
-create or replace synonym input_data for day??_example;
+create or replace synonym input_data for day05_example;
 
 select * from input_data;
 

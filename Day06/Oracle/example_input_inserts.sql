a389b662bfd30b1278706f123c42e27737ec07fe
Steven Patenaude <spatenaude@netspend.com>
2022-12-19 17:30:40 -0600

    setup day 6

--- Day06/Oracle/example_input_inserts.sql
+++ Day06/Oracle/example_input_inserts.sql
@@ -1,12 +1,17 @@
 drop sequence line_number_sq;
 create sequence line_number_sq;
 
-drop table   day??_example;
-create table day??_example (lineno number, linevalue varchar2(4000));
+drop table   day06_example;
+create table day06_example (lineno number, linevalue varchar2(4000));
 
-create or replace synonym input_data for day??_example;
+create or replace synonym input_data for day06_example;
 
 --insert into input_data (lineno, linevalue) values (line_number_sq.nextval,'
 --');
+insert into input_data (lineno, linevalue) values (line_number_sq.nextval,'mjqjpqmgbljsphdztnvjfqwrcgsmlb');
+insert into input_data (lineno, linevalue) values (line_number_sq.nextval,'bvwbjplbgvbhsrlpgdmjqwftvncz');
+insert into input_data (lineno, linevalue) values (line_number_sq.nextval,'nppdvjthqldpwncqszvftbrmjlhg');
+insert into input_data (lineno, linevalue) values (line_number_sq.nextval,'nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg');
+insert into input_data (lineno, linevalue) values (line_number_sq.nextval,'zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw');
 
 commit;
--- Day06/Oracle/example_workbook.sql
+++ Day06/Oracle/example_workbook.sql
@@ -1,6 +1,10 @@
 -- scratchpad for creating the solution
-create or replace synonym input_data for day??_example;
+create or replace synonym input_data for day06_example;
 
 select * from input_data;
 
--- Sample
+-- input is only one line, the input data is multiple examples
+-- so starting with one line
+select 'mjqjpqmgbljsphdztnvjfqwrcgsmlb' from dual;
+
+-- find the first substring of 4 with unique letters

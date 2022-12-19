5d00a9c4636c27cf0021095662481d60f7e77ab4
jungwolf <spatenau@gmail.com>
2022-12-18 22:00:36 -0500

    move note to workbook

--- Day18/Oracle/example_workbook.sql
+++ Day18/Oracle/example_workbook.sql
@@ -3,4 +3,10 @@ create or replace synonym input_data for day18_example;
 
 select * from input_data;
 
--- Sample
+/*
+could just search for an adjacent cube, subtract one, repeat
+or, group (x,y),(x,z),(y,z) and count number of non consecutive columns
+eg at 5,5: 1,2 4 9 would be three groups, so three columns with 2 sides each
+*/
+
+

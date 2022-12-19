06b58b9cd6d42af50f3d723925a73edad50f23a8
jungwolf <spatenau@gmail.com>
2022-12-18 21:59:11 -0500

    update template files to use drop if exists

--- misc/Day_template/Oracle/example_input_inserts.sql
+++ misc/Day_template/Oracle/example_input_inserts.sql
@@ -1,7 +1,7 @@
-drop sequence line_number_sq;
+exec drop_object_if_exists('line_number_sq','sequence');
 create sequence line_number_sq;
 
-drop table   day??_example;
+exec drop_object_if_exists('day??_example','table');
 create table day??_example (lineno number, linevalue varchar2(4000));
 
 create or replace synonym input_data for day??_example;
